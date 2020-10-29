7b5ba4ee7225eb6f6d92324eb6e5ae72221617a7
linaressergio <42595954+linaressergio@users.noreply.github.com>
2020-10-29 08:01:06 -0500

    Update client.py

--- service/client.py
+++ service/client.py
@@ -1,32 +1,35 @@
 """ Class representing a client
     """
 class Client():
-
+""" Class representing a client"""
     def __init__(self, name, last_name, doc_id):
+        """ init client"""
         self.name = name
         self.last_name = last_name
         self.doc_id = doc_id
         self.preexistence = []
 
-    def add_preexistence(self, nPreexistence):
-        self.preexistence.append(nPreexistence)
+    def add_preexistence(self, n_preexistence):
+        """ Add a preexistances"""
+        self.preexistence.append(n_preexistence)
         return len(self.preexistence) - 1
 
     def get_preexistence(self, p_Index):
         """ Get a preexistance given the index"""
         if p_Index >= len(self.preexistence):
             return 'There is no such preexistence'
-        else:
-            return self.preexistence[p_Index]
 
     def get_all_preexistence(self):
+        """ Get all preexistances"""
         return self.preexistence
 
     def remove_preexistence(self, n_preexistence):
+        """ Get all preexistances"""
         self.preexistence.pop(n_preexistence)
         return len(self.preexistence) - 1
 
     def get_formatted_name(self):
+        """ Get full name"""
         return self.name + ' ' + self.last_name
 
 
